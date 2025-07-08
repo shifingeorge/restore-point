67009f194613352962bcfd71d3aad3904615f3f0
shifiyy <shifiyy03.design@gmail.com>
2025-07-08 07:01:25 +0000

    added refer link on copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -87,8 +87,17 @@ const Footer = () => {
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
             <div className="text-center">
               <p className="text-gray-400 text-sm">
-                © 2025 All rights reserved | Designed By Nainas Digital Advertising Group
+                © 2025 All rights reserved | Designed By{' '}
+                <a 
+                href="https://www.nainasdigital.com" 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                className="text-[#f89535] hover:text-[#e67c1f] transition-colors duration-300"
+                >
+                  Nainas Digital Advertising Group
+                </a>
               </p>
+
             </div>
           </div>
         </div>
